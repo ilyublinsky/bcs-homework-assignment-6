66978868266b9734c516bfe621f31782ed381a30
Ingrid Lyublinsky <ingrid.lyub@gmail.com>
2021-02-04 18:14:46 -0800

    initial js and erasure of some html

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 // Different ways to use the search option. //
-var APIKEY = "";
-var city = "";
-var date = "";
-var condition = "";
-var temperature = "";
-var humidity = "";
-var uvIndex = "";
+var APIKEY = "5d5c7866dcc54075c141f06a9dea4f1b";
+var requestUrl = ('https://api.openweathermap.org/data/2.5/weather?id=');
+var city = $("");
+var date = $("");
+var temperature = $("terperature");
+var windSpeed = $("wind");
+var humidity = $("humidity");
+var uvIndex = $("uvLight");
 
+// Calling the search function
+$(document).ready(function() {
+  $("#search").on("click", function() {
+    var searchResults = $('#search').value();
+    console.log(('#search').value());
+  }
+}
 
-$(document).ready(function(){
-  $("submit").click(function()
-});
-
+function weather-condition(city) {
+  var APIKEY = '{5d5c7866dcc54075c141f06a9dea4f1b}';
+  fetch('https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?' + city+ '&appid=' + APIKEY)  
+  .then(function(resp) { 
+    return resp.json() 
+  })
+  .then(function(data) {
+  })
+  .catch(function() {
+  });
+}
