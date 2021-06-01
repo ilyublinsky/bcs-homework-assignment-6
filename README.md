9b2d3abea181f7d4e83b734f4e80f3a6a51ad22e
Ingrid Lyublinsky <ingrid.lyub@gmail.com>
2021-06-01 11:50:34 -0700

    Readme mockup

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# 06 Server-Side APIs: Weather Dashboard
+# Five Day Weather Dashboard
 
-## Your Task
+## Description & App
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+Weather Dashboard APP have a dinamically updated Html and CSS Bootstrap framework that using your browser local storage to save the data from users and API call. The user can search for the city weather all arround the world and display the current weather and 5 days forecast tith all weather conditions. The past city searched is saved in colomn so it can be access by user without typing again.
 
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that provides basic setup and usage instructions. You will use `localStorage` to store any persistent data.
+[Five Day Weather Dashboard](https://ilyublinsky.github.io/five-day-weather-app-bcs6/)
 
 ## User Story
 
@@ -30,61 +30,16 @@ WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
 ```
 
-## Mock-Up
+## Demo
 
 The following image shows the web application's appearance and functionality:
 
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
+![ScreenShot]()
 
-## Grading Requirements
+## Contributors
 
-This homework is graded based on the following criteria: 
+Ingrid Lyublinsky
 
-### Technical Acceptance Criteria: 40%
+[GitHub](https://github.com/ilyublinsky)
 
-* Satisfies all of the above acceptance criteria plus the following:
-
-    * Uses the OpenWeather API to retrieve weather data.
-
-    * Uses `localStorage` to store persistent data.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+> ingrid.lyub@gmail.com
--- script.js
+++ script.js
@@ -2,28 +2,165 @@
 var APIKEY = "5d5c7866dcc54075c141f06a9dea4f1b";
 var requestUrl = ('https://api.openweathermap.org/data/2.5/weather?id=');
 var city = $("");
+var search = $("#search-button");
 var date = $("");
-var temperature = $("terperature");
-var windSpeed = $("wind");
-var humidity = $("humidity");
-var uvIndex = $("uvLight");
+var temperature = $("#terperature");
+var icon = $("#weatherIcon");
+var windSpeed = $("#wind");
+var humidity = $("#humidity");
+var uvIndex = $("#uvLight");
+var currentWeatherResults = $("#results");
+var forecastDisplay = $("#fiveDayForecast");
+var cityResults;
+
+if (localStorage.getItem("localWeatherSearches")) {
+    citiesArray = JSON.parse(localStorage.getItem("localWeatherSearches"));
+    writeSearchHistory(citiesResults);
+} else {
+    citiesResults = [];
+};
+
+
+function weatherConditions(cityName) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+
+    $.get(queryURL).then(function(response){
+        let date = new Date(response.dt*1000);
+        let icon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+
+        currentWeatherResults.html(`
+        <h3>${response.name}, 
+        ${response.sys.country} 
+        (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})
+        <img src=${weatherIcon}></h3>
+        <p>Temperature: ${response.main.temp} &#176;C</p>
+        <p>Humidity: ${response.main.humidity}%</p>
+        <p>Wind Speed: ${response.wind.speed} m/s</p>
+        `, returnUVIndex(response.coord))
+        createHistoryButton(response.name);
+    })
+};
+
+function returnWeatherForecast(cityName) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&APPID=${apiKey}`;
+
+    $.get(queryURL).then(function(response){
+        let forecastInfo = response.list;
+        forecastDiv.empty();
+        $.each(forecastInfo, function(i) {
+            if (!forecastInfo[i].dt_txt.includes("12:00:00")) {
+                return;
+            }
+            let forecastDate = new Date(forecastInfo[i].dt*1000);
+            let weatherIcon = `https://openweathermap.org/img/wn/${forecastInfo[i].weather[0].icon}.png`;
+
+            forecastDiv.append(`
+            <div class="col-md">
+                <div class="card text-white bg-primary">
+                    <div class="card-body">
+                        <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
+                        <img src=${weatherIcon} alt="Icon">
+                        <p>Temp: ${forecastInfo[i].main.temp} &#176;C</p>
+                        <p>Humidity: ${forecastInfo[i].main.humidity}%</p>
+                    </div>
+                </div>
+            </div>
+            `)
+        })
+    })
+};
+
+// The current UV index is collected at the same time as the current weather
+// by making use of the searched city's returned coordinates
+function returnUVIndex(coordinates) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${coordinates.lat}&lon=${coordinates.lon}&APPID=${apiKey}`;
+
+    $.get(queryURL).then(function(response){
+        let currUVIndex = response.value;
+        let uvSeverity = "green";
+        let textColour = "white"
+        //Change UV background based on severity
+        //Also change text colour for readability
+        if (currUVIndex >= 11) {
+            uvSeverity = "purple";
+        } else if (currUVIndex >= 8) {
+            uvSeverity = "red";
+        } else if (currUVIndex >= 6) {
+            uvSeverity = "orange";
+            textColour = "black"
+        } else if (currUVIndex >= 3) {
+            uvSeverity = "yellow";
+            textColour = "black"
+        }
+        currWeatherDiv.append(`<p>UV Index: <span class="text-${textColour} uvPadding" style="background-color: ${uvSeverity};">${currUVIndex}</span></p>`);
+    })
+}
+
+function createHistoryButton(cityName) {
+    // Check if the button exists in history, and if it does, exit the function
+    var citySearch = cityName.trim();
+    var buttonCheck = $(`#previousSearch > BUTTON[value='${citySearch}']`);
+    if (buttonCheck.length == 1) {
+      return;
+    }
+    
+    if (!citiesArray.includes(cityName)){
+        citiesArray.push(cityName);
+        localStorage.setItem("localWeatherSearches", JSON.stringify(citiesArray));
+    }
+
+    $("#previousSearch").prepend(`
+    <button class="btn btn-light cityHistoryBtn" value='${cityName}'>${cityName}</button>
+    `);
+}
+
+function writeSearchHistory(array) {
+    $.each(array, function(i) {
+        createHistoryButton(array[i]);
+    })
+}
+
+// Get a deafult weather search
+returnCurrentWeather("Toronto");
+returnWeatherForecast("Toronto");
+
+$("#submitCity").click(function() {
+    event.preventDefault();
+    let cityName = $("#cityInput").val();
+    returnCurrentWeather(cityName);
+    returnWeatherForecast(cityName);
+});
+
+$("#previousSearch").click(function() {
+    let cityName = event.target.value;
+    returnCurrentWeather(cityName);
+    returnWeatherForecast(cityName);
+})
+
 
 // Calling the search function
-$(document).ready(function() {
-  $("#search").on("click", function() {
-    var searchResults = $('#search').value();
-    console.log(('#search').value());
-  }
-}
+// $(document).ready(function() {
+//   $("#submit").on("click", function() {
+//     var searchResults = $('#search').value();
+//     console.log(('#search').value());
+//   }
+// }
+//Adding loop for storage
+// for (var i = 0; i < localStorage.length; i++) {
+//   var city = localStorage.getItem(i);
+//   var cityName = $(".list-group").addClass("list-group-item");
+// }
 
-function weather-condition(city) {
-  var APIKEY = '{5d5c7866dcc54075c141f06a9dea4f1b}';
-  fetch('https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?' + city+ '&appid=' + APIKEY)  
-  .then(function(resp) { 
-    return resp.json() 
-  })
-  .then(function(data) {
-  })
-  .catch(function() {
-  });
-}
+//   cityName.append("<li>" + city + "</li>");
+// function weatherCondition(city) {
+//   var APIKEY = '{5d5c7866dcc54075c141f06a9dea4f1b}';
+//   fetch('https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?' + city+ '&appid=' + APIKEY)  
+//   .then(function(resp) { 
+//     return resp.json() 
+//   })
+//   .then(function(data) {
+//   })
+//   .catch(function() {
+//   })
+//   console.log(data);
+// }
